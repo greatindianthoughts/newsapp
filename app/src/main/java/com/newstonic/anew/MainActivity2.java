dcc9eb1e36d8fce38bfc68c43a1d6c3ac78d4c85
greatindianthoughts <rudreshpamidy@gmail.com>
2022-11-01 18:36:32 +0530

    News Tonic Android application Project Fineshed

---
+++ app/src/main/java/com/newstonic/anew/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.newstonic.anew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+}
